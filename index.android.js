65611b7c58dd2749294e0ec794050441743e94c5
Sylvain Cousquer <scousquer@talentsoft.com>
2017-05-04 15:46:38 +0200

    simple header component

--- index.android.js
+++ index.android.js
@@ -2,13 +2,16 @@
 
 // Import a library to help create a componant
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
 
 // Create a component
 const App = () => (
-    <Text>Some Text</Text>
+    <Header />
 );
-  
+
+
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);

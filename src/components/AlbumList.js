8c73cebcfdabd1cc5afd05189b39359282d5b21f
Sylvain Cousquer <scousquer@talentsoft.com>
2017-05-12 15:47:00 +0200

    Add scrolling capability

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
-state = { albums: [] };
+    state = { albums: [] };
 
-componentWillMount() {
-    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-        .then(response => this.setState({ albums: response.data }));
-}
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
 
-renderAlbums() {
-    return this.state.albums.map(album => 
-        <AlbumDetail key={album.title} album={album} />
-    );
-}
+    renderAlbums() {
+        return this.state.albums.map(album =>
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
 
-render() {
+    render() {
         console.log(this.state);
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 }

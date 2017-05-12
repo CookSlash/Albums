4ccd8708aec7d597330c62695d1dc0d00f3c79a6
Sylvain Cousquer <scousquer@talentsoft.com>
2017-05-12 17:06:30 +0200

    Setting text on button by props

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 
-const Button = ({ onPress }) => {
+const Button = ({ onPress, children }) => {
 
     const { buttonStyle, textStyle } = styles;
 
     return (
         <TouchableOpacity onPress={onPress} style={buttonStyle}>
             <Text style={textStyle}>
-                Click me!!!
-             </Text>
+                {children}
+            </Text>
         </TouchableOpacity>
     );
 };
